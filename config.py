efb77ea2d00020f65c454ae96c58444a69ef202b
Vemrito <96917413+Vemrito@users.noreply.github.com>
2022-02-22 16:25:45 +0300

    Телеграм бот
    Конвертер валют

---
+++ config.py
@@ -0,0 +1,7 @@
+
+TOKEN = '5242874114:AAGD1zKnl69bBEEj8E32-UFRQiQhe4ai9VI'
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар' : 'USD',
+}
---
+++ extensions.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from config import keys
+class ConvertionException(Exception):
+    pass
+class CryptoConverter:
+    @staticmethod
+    def convert(quote:str,base: str, amount:str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработатьь валюту {quote}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = float(json.loads(r.content)[base_ticker]) * amount
+        return total_base
